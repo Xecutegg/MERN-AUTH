3c54c36ae3b51698dacb1dded948db38d0832074
Xecutegg <soumogoswami.2003@gmail.com>
2025-03-28 22:26:59 +0530

    logo update

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,14 +1,16 @@
 import arrow_icon from './arrow_icon.svg'
 import lock_icon from './lock_icon.svg'
-import logo from './logo.svg'
+import logo1 from './logo1.svg'
 import mail_icon from './mail_icon.svg'
 import person_icon from './person_icon.svg'
 import hand_wave from './hand_wave.png'
 import header_img from './header_img.png'
+import logo from './logo.png'
 
 export const assets = {
     arrow_icon,
     lock_icon,
+    logo1,
     logo,
     mail_icon,
     person_icon,
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -60,7 +60,7 @@ const Login = () => {
 
     return (
         <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-300 to-purple-300'>
-            <img onClick={() => navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
+            <img onClick={() => navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-25 cursor-pointer' />
             <div className='bg-slate-900 p-10 rounded-lg w-full sm:w-96 text-indigo-300 text-sm'>
 
                 <h2 className='text-3xl font-semibold text-white text-center mb-3 '>{state === 'Sign up' ? 'Create account' : 'Login'}</h2>
